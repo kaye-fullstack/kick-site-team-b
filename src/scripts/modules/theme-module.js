c9ca7ab3c2edbf0d5ad07135225b9bbd43a2c81c
jhemary-fullstack <81550353+jhemary-fullstack@users.noreply.github.com>
2021-03-30 09:17:56 +0800

    header-done (#17)
    * header-done
    
    * fixed header tab spacing issue
    
    Co-authored-by: Jhemary <jhemarytalon18@gmail.com>

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -22,6 +22,19 @@ AppName.Modules.ThemeModule = (function () {
     _privateMethod();
   };
 
+const elem = $('header');
+const scrolled = () => {
+ const threshold = $(document).scrollTop() > 50;
+ elem.toggleClass('scrolled', threshold);
+ };
+$(window).on({ scroll: scrolled });
+$('body').css({'overflow':'hidden'});
+$(document).bind('scroll',function () { 
+     window.scrollTo(0,0); 
+});
+$(document).unbind('scroll'); 
+$('body').css({'overflow':'visible'});
+
   return {
     init: init,
   };
